48b28cd8b44495dc3f41eb1b53846ab00be0e2f3
Ruthietuti <80335371+Ruthietuti@users.noreply.github.com>
2022-05-28 20:43:20 +0100

    reading text task

---
+++ Reading-Text-Files/main.py
@@ -0,0 +1,22 @@
+# Read text from a file, and count the occurrence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!")
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+
+
+def read_file_content(filename) :
+    with open(filename) as d:
+        file = d.read()
+        return file
+
+def count_words():
+    text = read_file_content("./story.txt")
+    count = dict()
+    words = text.split()
+    for word in words:
+        if word in count:
+            count[word] = +1
+        else:
+            count[word] = 1
+    return (count)
+print(count_words())
